16fb93d6f5d09c5a153233bdf9a01f48f0dc10d8
Edgar Pino <edgarbeltran@me.com>
2018-02-26 12:30:39 -0700

    return

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -48,6 +48,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             
             base.CalculateStatistics();
@@ -58,6 +59,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.Write("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             
             base.CalculateStudentStatistics(name);
